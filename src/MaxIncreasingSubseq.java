a4cb78edecbc316f52df7aee7a9d836495c58dd5
Spencer6497 <peaces2@mymail.nku.edu>
2019-09-10 15:31:23 -0400

    Initial commit

---
+++ src/MaxIncreasingSubseq.java
@@ -0,0 +1,16 @@
+/*
+    Spencer Peace
+    CSC 364-001
+    HW2: Dynamic Programming
+    Dr. Jeff Ward
+    This program prompts the user for a string and displays a maximum length increasing subsequence of characters.
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class MaxIncreasingSubseq {
+    public static void main(String[] args) {
+
+    }
+}
