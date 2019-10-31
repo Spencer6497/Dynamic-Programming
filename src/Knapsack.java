f97133b77efac952498ac38c3c9c3477ac129498
Spencer6497 <47868304+Spencer6497@users.noreply.github.com>
2019-10-31 16:40:12 -0400

    Update Knapsack.java
    Rework comments

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,9 +1,4 @@
 /*
-    Spencer Peace
-    CSC 364-001
-    HW2: Dynamic Programming
-    Dr. Jeff Ward
-
     This program prompts the user for a number of work weeks available, the name of the
     input and output files, reads the available projects from the input file, solves the
     corresponding knapsack problem (without repetition), and writes to the output file
